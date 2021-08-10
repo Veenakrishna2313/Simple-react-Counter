86d731668159bf4abf169905872e15b93edae4e7
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2021-08-09 22:32:14 -0700

    decrement button added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,6 +19,11 @@ class Counter extends Component {
     this.setState({count:this.state.count+1});
 
   }
+
+  handleDecrement =()=>{
+    this.setState({count:this.state.count-1});
+
+  }
   render() { 
 
    
@@ -27,7 +32,8 @@ class Counter extends Component {
     <div>
       
       <span style ={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+      <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-3">Increment</button>
+       <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-3">Decrement</button>
 
       
 
