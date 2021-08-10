1c3e6f08034df618669f1c94311af72bc79848fe
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2021-08-09 18:57:34 -0700

    counter button and styles created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counter';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Counter></Counter>
     </div>
   );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
 
 
 class Counter extends Component {
-  state = {  }
+  state = {  
+    count:0,
+
+  };
+
+  styles={
+
+    fontSize:'15px',
+    fontWeight: "Bold"
+  }
+
   render() { 
-    return <h1>Hello World</h1>;
+
+   
+
+    return (
+    <div>
+      
+      <span style ={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <button className="btn btn-secondary btn-sm">Increment</button>
+
+    </div>
+    );
+    
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-3 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount(){
+    // obj deconstruction
+
+    const {count}= this.state;
+
+    return count===0?"Zero" : count;
   }
 }
  
