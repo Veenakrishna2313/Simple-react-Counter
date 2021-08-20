880b01577f09a246256cfc7ac3f3ecbc7c05d918
Veena Krishna Sandana <veena@theinformedvoterproject.org>
2021-08-19 23:08:35 -0700

    Styling Counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,13 +18,19 @@ class Counter extends Component {
    
  
     return (
-    <div>
-      {this.props.children}
-      <span style ={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button onClick={()=>this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm m-3">Increment</button>
-       <button onClick={()=>this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm m-3">Decrement</button>
-       <button onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-3">Delete</button>
-
+    <div className="row">
+      <div className="col-1">
+        <span style ={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      </div>
+      <div className="col">
+        <button onClick={()=>this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm ">+</button>
+       <button onClick={()=>this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm m-2">-</button>
+       <button onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm">x</button>
+</div>
+      
+  
+      
+      
       
 
     </div>
